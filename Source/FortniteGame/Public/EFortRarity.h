f86054fec47ec2ab68a9aa9c9b482818b47dacd4
Zylox <76428709+zyloxmods@users.noreply.github.com>
2024-03-25 01:55:44 +0100

    Update EFortRarity.h for 8.51 format

--- Source/FortniteGame/Public/EFortRarity.h
+++ Source/FortniteGame/Public/EFortRarity.h
@@ -3,15 +3,19 @@
 #include "EFortRarity.generated.h"
 
 UENUM(BlueprintType)
-enum class EFortRarity : uint8 {
-    Common,
-    Uncommon,
-    Rare,
-    Epic,
-    Legendary,
-    Mythic,
-    Transcendent,
-    Unattainable,
-    NumRarityValues,
+enum class EFortRarity : uint8
+{
+    Handmade = 1 UMETA(DisplayName = "Common (Grey)"),
+    Ordinary = 0 UMETA(DisplayName = "Uncommon (Green)"),
+    Sturdy = 2 UMETA(DisplayName = "Rare (Blue)"),
+    Quality = 3 UMETA(DisplayName = "Epic (Purple)"),
+    Fine = 4 UMETA(DisplayName = "Legendary (Orange)"),
+    Elegant = 5 UMETA(DisplayName = "Mythic (Yellow)"),
+    Masterwork = 6 UMETA(DisplayName = "Transcendent (Pinkish)"),
+    Epic = 7 UMETA(DisplayName = "Impossible (T07)"),
+    Badass = 8 UMETA(DisplayName = "Impossible (T08)"),
+    Legendary = 9 UMETA(DisplayName = "Impossible (T09)"),
+    NumRarityValues = 10 UMETA(Hidden),
+    EFortRarity_MAX = 11 UMETA(Hidden),
 };
 
