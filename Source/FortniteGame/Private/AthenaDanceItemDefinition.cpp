2dadabd88d6ae52b3469a54fe04bed5ac3f32106
Zylox <76428709+zyloxmods@users.noreply.github.com>
2024-04-28 18:50:43 +0200

    Update AthenaDanceItemDefinition.cpp

--- Source/FortniteGame/Private/AthenaDanceItemDefinition.cpp
+++ Source/FortniteGame/Private/AthenaDanceItemDefinition.cpp
@@ -9,6 +9,8 @@ UAthenaDanceItemDefinition::UAthenaDanceItemDefinition() {
     this->bMoveForwardOnly = false;
     this->WalkForwardSpeed = 300.00f;
     ItemType = EFortItemType::AthenaDance;
+    UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+    Manager.AddNativeGameplayTag(TEXT("Cosmetics.EmoteType.Dance"));
     GameplayTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Cosmetics.EmoteType.Dance")));
 }
 
